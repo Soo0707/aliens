aa8f56118f86d9bd8bed06a9e49ddcd4f1362eef
Nabil <nabilirfan1987@gmail.com>
2025-04-25 15:24:25 +0800

    very_basic_individual_enemy_class

---
+++ code/drunkard.py
@@ -0,0 +1,9 @@
+import pygame
+from os import walk
+from os.path import join
+from player import  *
+from enemy import *
+
+class Trapper(Enemy):
+    def __init__(self, player, groups, collide, location, attack):
+        super().__init__(groups)
