08d4351c61b95562b6b8601e71c7001f98df0ec0
Niilesh <n11lesh.sk@gmail.com>
2025-04-25 22:32:19 +0800

    attempted to fix collisions, need to fix even more now , im cooked

--- code/enemy.py
+++ code/enemy.py
@@ -6,15 +6,15 @@ from xp import *
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, player, groups, collide, xp, location, enemies, attack):
+    def __init__(self, player, groups, collide, location, enemies, attack, xp, health, all_sprites):
         super().__init__(groups)
-
+        
         self.image = pygame.image.load(join("..", "assets", "enemy", "Trapper" , "1.png")).convert_alpha() #need to change this later to fit with animations
         self.rect = self.image.get_frect(center = location)
 
 
         self.location = (500,500)
-        self.health = 50
+        self.health = health
 
         self.speed = 150 # Enemy
         self.attack = 10 #Enemy attack 
@@ -23,12 +23,12 @@ class Enemy(pygame.sprite.Sprite):
         self.last_attack = 0
         self.can_attack = True
 
-        
         self.player = player
-        self.xp = xp
+
         self.collidables = collide
         self.enemies = enemies
-        
+        self.all_sprites = all_sprites
+        self.xp = xp
 
 
     def movement(self, dt):
@@ -40,14 +40,43 @@ class Enemy(pygame.sprite.Sprite):
         self.direction = (player_pos - enemy_pos).normalize()
 
         #movement
-        #Move on X axis
+
+        collision_list = [collidable.rect for collidable in self.collidables] + [self.player.rect]
+
+        #X-AXIS
         self.rect.x += self.direction.x * self.speed * dt
+
+        #Map Collision
         for collidable in self.collidables:
             if self.rect.colliderect(collidable.rect):
                 if self.direction.x > 0:  
                     self.rect.right = collidable.rect.left
                 elif self.direction.x < 0:  
                     self.rect.left = collidable.rect.right
+
+        #Player Collisions
+        if self.rect.colliderect(self.player.rect):
+            if self.direction.x > 0:  
+                self.rect.right = self.player.rect.left
+            elif self.direction.x < 0:  
+                self.rect.left = self.player.rect.right
+
+
+
+        #Enemy Collision
+        for nemesis in self.enemies:
+            if nemesis == self:
+                continue  # Skip self
+
+            if self.rect.colliderect(nemesis.rect):
+                if self.direction.x > 0:  
+                    self.rect.right = nemesis.rect.left
+                elif self.direction.x < 0:  
+                    self.rect.left = nemesis.rect.right
+
+
+        #Y - AXIS
+
 
         # Move on Y axis
         self.rect.y += self.direction.y * self.speed * dt
@@ -58,34 +87,16 @@ class Enemy(pygame.sprite.Sprite):
                 elif self.direction.y < 0:  
                     self.rect.top = collidable.rect.bottom
 
-        #Player Collisions
-        #X-Axis
-        if self.rect.colliderect(self.player.rect):
-            if self.direction.x > 0:  
-                self.rect.right = self.player.rect.left
-            elif self.direction.x < 0:  
-                self.rect.left = self.player.rect.right
-
-        #Y-Axis
         if self.rect.colliderect(self.player.rect):
             if self.direction.y > 0:  
                 self.rect.bottom = self.player.rect.top
             elif self.direction.y < 0:  
                 self.rect.top = self.player.rect.bottom
 
-
         #Enemy Collisions
-
         for nemesis in self.enemies:
             if nemesis == self:
                 continue  # Skip self
-
-            # X-Axis collision
-            if self.rect.colliderect(nemesis.rect):
-                if self.direction.x > 0:  
-                    self.rect.right = nemesis.rect.left
-                elif self.direction.x < 0:  
-                    self.rect.left = nemesis.rect.right
 
             # Y-Axis collision
             if self.rect.colliderect(nemesis.rect):
@@ -103,12 +114,11 @@ class Enemy(pygame.sprite.Sprite):
 
         if self.can_attack and self.rect.colliderect(self.player.rect):
             self.player.health = self.player.health - self.attack
-            self.health = self.health - 50
             self.can_attack = False
             self.last_attack = now
 
         if self.health <= 0:
-            Orb(self.rect.center, self.groups(), self.xp)
+            Orb(self.rect.center, (self.all_sprites, self.xp))
             self.kill()
         
 
@@ -122,4 +132,4 @@ class Enemy(pygame.sprite.Sprite):
 
         self.le_attack()
         self.movement(dt)
-        print(self.player.health)
+        print(self.health)
