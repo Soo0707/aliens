2efb13d25ea03fe9aeec285dac2a6612c5aa0eab
Nabil <nabilirfan1987@gmail.com>
2025-04-27 22:13:12 +0800

    succesfully created a menu for powerups

--- code/main.py
+++ code/main.py
@@ -26,7 +26,7 @@ class game():
         self.enemies = pygame.sprite.Group()
         
         #ui 
-        self.ui = Menu()
+        self.ui = UI()
 
         self.setup()
     
--- code/ui.py
+++ code/ui.py
@@ -1,15 +1,32 @@
+import pygame
+pygame.font.init()
 from main import *
 
 class UI:
     def __init__(self): #add powerup as attribute when done
         self.display_surface = pygame.display.get_surface()
-        self.font = pygame.font.Font(None, 30)
-        self.left = 1280 / 2 
-        self.top = 720 /2
+        self.font = pygame.font.Font(None, 40)
+        self.left = 215
+        self.top = 85
         #self.powerup = powerup
         
     def general(self):
-        print('general')
-    
+        #background
+        rect = pygame.FRect(self.left, self.top, 850, 550)
+        pygame.draw.rect(self.display_surface, 'white', rect, 0, 4)
+        pygame.draw.rect(self.display_surface, 'gray', rect, 4, 4)
+        
+        #menu
+        cols, rows = 1, 2
+        for col in range(cols):
+            for row in range(rows):
+                x = rect.left + rect.width / 4 
+                y = rect.top + (rect.height / 4) + (rect.height / 2) * row
+                text_surf = self.font.render('option', True, 'black')
+                text_rect = text_surf.get_frect(center = (x,y))
+                self.display_surface.blit(text_surf, text_rect)
+                
+                
+        
     def draw(self):
         self.general()
