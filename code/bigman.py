96f4b66450a4a5d1411f3991e297c61770b7cdef
SOO <ssj060707+github@gmail.com>
2025-06-05 22:28:47 +0800

    Bug fix: reset spawner timeout after death

--- code/bigman.py
+++ code/bigman.py
@@ -3,8 +3,8 @@ from enemy import *
 class BigMan(Enemy):
     def __init__(self, player, state, location, powerups, textures, xp_texture, xp_group, all_sprites_group, groups):
         super().__init__(player, state, location, powerups, xp_texture, xp_group, all_sprites_group, groups)
-        self.speed = 100
-        self.health = 500
+        self.speed = 150
+        self.health = 1000
         self.images = textures["normal"]
         self.images_flash = textures["flash"]
         self.image = self.images[0]
