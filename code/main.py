a104ee0af54517a7d2adacc3fd6227caee31361a
SOO <ssj060707+github@gmail.com>
2025-06-12 15:41:42 +0800

    Fixed if condition

--- code/main.py
+++ code/main.py
@@ -506,7 +506,7 @@ class game():
                         self.num_xp = 0
 
                         for spawner in self.spawners_group:
-                            if spawner.timeout_ticks > 1000:
+                            if spawner.timeout_ticks > 0:
                                 spawner.timeout_ticks -= self.level_up * 5
 
                             if spawner.timeout_ticks < 0:
